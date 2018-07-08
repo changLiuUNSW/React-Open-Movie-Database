e4fd2f2499d760653bbf274027fe17214ed459cc
chang liu <3dslayer@gmail.com>
2018-07-08 20:06:33 +1000

    update title

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -22,7 +22,7 @@ const Title = styled(Link)`
 const Nav = () => {
   return (
     <Container>
-      <Title to="/">Open Movie Database</Title>
+      <Title to="/">React Open Movie Database</Title>
     </Container>
   );
 };
